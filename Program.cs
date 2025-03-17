766e9196f39eb5781dce0489f7cb4be04d8bae7e
LAB53 <lab53@fatec.com>
2025-03-17 09:39:12 -0300

    pergunta o nome e sobrenome do usuário

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string nome;
+string sobrenome;
+
+Console.Clear();
+
+Console.Write("Digite seu primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome do catálogo: {sobrenome.ToUpper()}, {nome}");
